6d48683a9f2cd2533187a6f7191ce58c8270ee73
Mael Donnart <maeldonnart@hotmail.fr>
2024-03-31 13:04:32 +0200

    - Finalisation de la transition de text-container dans le component Collapse.

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -3,11 +3,10 @@ import './collapse.css'
 import arrow from '../../../public/arrow.svg'
 
 function Collapse({title, text, customClass}){
-    const [isClosed, setIsClose] = useState(true)
-    const [isWrap, setIsWrap] = useState(false)
+    const [isClosed, setIsClosed] = useState(true)
 
     function setAnimating() {
-        setIsClose(!isClosed);
+        setIsClosed(!isClosed);
     }
 
 
@@ -16,7 +15,7 @@ function Collapse({title, text, customClass}){
         <div className='collapse-container'>
             <div className='collapse-box'>
                 <h2>{title}</h2>
-                <img className={`arrow ${isClosed ? 'rotate-arrow' : ''}`} onClick={setAnimating} src={arrow} alt='icone de flèche'/>
+                <img className={`arrow ${isClosed ? 'rotate-arrow' : ''}`} onClick={setAnimating} src={arrow} alt='icone de flèche'  aria-expanded={!isClosed}/>
             </div>
             <div className={`text-container ${isClosed ? 'hidden' : ''}`} >
                 <p>{text}</p>
