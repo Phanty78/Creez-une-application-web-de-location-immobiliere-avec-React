9524020863770860de491e1712efe3b2edbd95c1
Mael Donnart <maeldonnart@hotmail.fr>
2024-03-31 16:01:25 +0200

    - Ajout de la gestion du cas spécifique de la reception d'un array via la prop text dans le composant collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -9,17 +9,24 @@ function Collapse({title, text, customClass}){
         setIsClosed(!isClosed);
     }
 
-
-
     return (
         <div className='collapse-container'>
             <div className='collapse-box'>
                 <h2>{title}</h2>
                 <img className={`arrow ${isClosed ? 'rotate-arrow' : ''}`} onClick={setAnimating} src={arrow} alt='icone de flèche'  aria-expanded={!isClosed}/>
             </div>
+            { Array.isArray(text) ?
+                <div className={`text-container ${isClosed ? 'hidden' : ''}`}>
+                    <ul>
+                        {text.map((element,index) =>
+                            <li key={index}>{element}</li>
+                        )}
+                    </ul>
+                </div>
+            : 
             <div className={`text-container ${isClosed ? 'hidden' : ''}`} >
                 <p>{text}</p>
-            </div>
+            </div>}
         </div> 
     )
 }
