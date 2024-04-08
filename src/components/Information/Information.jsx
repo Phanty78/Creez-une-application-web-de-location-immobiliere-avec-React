b27254fdf265dba8ad498e5cf29490e0c514ea73
Mael Donnart <maeldonnart@hotmail.fr>
2024-04-08 14:52:38 +0200

    - Modification du component Tag et Informations et de leur SCSS

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -14,7 +14,11 @@ function Information({ title, description, rating, host, location, tags, equipme
                     <p className='title'>{title}</p>
                     <p className='location'>{location}</p>
                 </div>
-                <Tag tags={tags} />
+                <div className='tags-container'>
+                {tags.map((tag, index) => {
+                    return <Tag key={index} tag={tag} />
+                })}
+                </div>
             </div>
             <div className='host-and-ratings-container'>
                 <div className='host-container'>
--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,20 +1,12 @@
 import './tag.css'
 import PropTypes from 'prop-types';
 
-function Tag({tags}){
-    return (
-        <div className='tags-container'>
-            {tags.map((tag, index) => (
-                    <div key={index} className='tag-box'>{tag}</div>
-            )
-                    )}
-        </div>     
-           )
-    
+function Tag({ tag }) {
+    return <div className='tag-box'>{tag}</div>      
 }
 
 Tag.propTypes = {
-    tags : PropTypes.arrayOf(PropTypes.string).isRequired
+    tag: PropTypes.string.isRequired
 }
 
 export default Tag
